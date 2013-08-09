89934b68fb3f4d78ab4cdfb00724ff2a57f7c083
Christopher Coker <chris@brightball.com>
2013-08-08 22:03:07 -0400

    remove the need to define the extension in the configuration
    Why not parse out the extension of the file from the name instead of explicitly requiring a specific extension in the configuration? By parsing out the extension you can use whatever extension you want whenever you want.  You can even use multiple extensions for dust files in the same application if you wanted.

--- dustjs-require.js
+++ dustjs-require.js
@@ -16,7 +16,8 @@ define(
        */
       load: function(name, req, onload, config){
         req(['text!'+name], function(tpl){
-          var path = name.slice(0, -(config.extension.length));
+          var extension = name.substring(name.lastIndexOf('.'))
+          , path = name.slice(0, -(extension.length));
           dust.loadSource(dust.compile(tpl, path));
           onload(path);
         });
